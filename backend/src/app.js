879d229ec1e2a193f10edf84f9e9660b73b29b45
Ayush Ghole <ayushghole@gmail.com>
2025-12-29 20:20:37 +0530

    Fixing the frontend routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://beyound-chats-roan.vercel.app/",
+    origin: "https://beyound-chats-roan.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
